06f64d1475175c025b539935d150e649497879bd
Isaaciligaray <145177651+Isaaciligaray@users.noreply.github.com>
2024-06-26 21:19:39 -0400

    Add files via upload

--- BDMonoV8.sql
+++ BDMonoV8.sql
@@ -4,8 +4,7 @@ use BDMonoV8;
 
 create table Clientes(
 	id_cliente int auto_increment primary key,
-    Nombre varchar(250) not null,
-    total_compra int(7)
+    Nombre varchar(250) not null
     );
 
 create table Facturas(
@@ -25,21 +24,36 @@ create table MIA_vendidos(
     foreign key (id_factura) references Facturas(id_factura)
     );
 
-Insert into Clientes values ("1" ,"Segutec", "399000");
+Insert into Clientes values ("1" ,"Segutec");
 
 Insert into Facturas values ("89", "1", "339000","2024-03-29");
 
 Insert into MIA_vendidos values ("MIA000040852453", "89", "127.79","4.46","33839");
 Insert into MIA_vendidos values ("MIA000040852501", "89", "146.77","7.24","50219");
 
-select*from mia_vendidos;
+select*from mia_vendidos order by id_factura asc;
 
 SELECT YEAR(fecha) AS Año, MONTH(fecha) AS Mes, sum(monto_total) AS "Total facturado"
 FROM Facturas
 GROUP BY YEAR(fecha), MONTH(fecha)
 ORDER BY Año, Mes;
 
-SELECT id_cliente, Nombre, SUM(total_compra) AS Suma_Compras
-FROM Clientes
-GROUP BY id_cliente, Nombre
-ORDER BY Suma_Compras DESC;
+SELECT DATE_FORMAT(fecha, '%Y-%m') AS 'Año-Mes', SUM(monto_total) AS 'Total facturado'
+FROM Facturas
+GROUP BY DATE_FORMAT(fecha, '%Y-%m')
+ORDER BY 'Año-Mes';
+
+select Nombre, Facturas.monto_total from Clientes where id_cliente = Facturas.id_cliente group by Nombre;
+
+SELECT c.Nombre, SUM(f.monto_total) AS Total_Compras
+FROM Clientes c
+JOIN Facturas f ON c.id_cliente = f.id_cliente
+GROUP BY c.Nombre
+ORDER BY Total_Compras DESC;
+
+SELECT c.Nombre, SUM(f.monto_total) AS Total_Compras
+FROM Clientes c
+JOIN Facturas f ON c.id_cliente = f.id_cliente
+GROUP BY c.Nombre
+ORDER BY Total_Compras DESC
+LIMIT 5;
